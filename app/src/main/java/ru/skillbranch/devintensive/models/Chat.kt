bdb5b13b41ac938cbf5b795c3f9c4c2e54a6fb02
Yura Yurchak <yurchaktest1@gmail.com>
2019-08-20 21:57:58 +0400

    Hometask_3 whit hometask2

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Chat.kt
@@ -0,0 +1,8 @@
+package ru.skillbranch.devintensive.models
+
+class Chat (
+    val id: String,
+    var members: MutableList<User> = mutableListOf(),
+    val message: MutableList<BaseMessage> = mutableListOf()
+) {
+}
